31e83f85a0dfb3ecb4eae534601ba550133ec315
furu8 <foooolurk1125@gmail.com>
2021-01-18 03:13:55 +0900

    refactor:path修正

--- 2020-11/scripts/make_image_data.py
+++ 2020-11/scripts/make_image_data.py
@@ -10,10 +10,12 @@ def load_image(path):
     npy_image_list = []
 
     for i, image in enumerate(image_list):
-        if i % 100 == 0:
+        if i % 100 == 0: # 雑に進行状況出力
             print(i)
+
         img = cv2.imread(image, cv2.IMREAD_COLOR) # デフォルトカラー読み込み
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # RGB変換
+        img = cv2.resize(img, (64, 64)) # リサイズ64x64
         # plt.imshow(img)
         # plt.show()
         # break
@@ -41,16 +43,16 @@ def convert_img2npy(load_path, save_path, isdir=False):
 
 
 def main():
-    FACE_LOAD_PATH = 'D:/Illust/Paimon/interim/face_only/*'                  # 顔画像のロードパス
-    FACE_SAVE_PATH = 'D:/Illust/Paimon/interim/npy_face_only/anime_face.npy' # 顔画像のセーブパス
-    FOOD_LOAD_PATH = 'D:/Illust/food-101/raw/images/*'                       # 飯画像のロードパス（ディレクトリごとに画像がわかれているため顔画像と若干パスが異なる）
-    FOOD_SAVE_PATH = 'D:/Illust/food-101/interim/npy_food-101/'              # 飯画像のセーブパス（ディレクトリごとに画像がわかれているため顔画像と若干パスが異なる）
+    FACE_LOAD_PATH = 'D:/Illust/Paimon/interim/face_only/*'                     # 顔画像のロードパス
+    FACE_SAVE_PATH = 'D:/Illust/Paimon/interim/npy_face_only/anime_face.npy'    # 顔画像のセーブパス
+    FOOD_LOAD_PATH = 'D:/Illust/food-101/raw/images/*/*'                        # 飯画像のロードパス
+    FOOD_SAVE_PATH = 'D:/Illust/food-101/interim/npy_food-101/npy_food-101.npy' # 飯画像のセーブパス
 
     # 顔画像をnpy形式に変換
     convert_img2npy(FACE_LOAD_PATH, FACE_SAVE_PATH)
 
     # 飯画像をnpy形式に変換
-    convert_img2npy(FOOD_LOAD_PATH, FOOD_SAVE_PATH, isdir=True)
+    convert_img2npy(FOOD_LOAD_PATH, FOOD_SAVE_PATH)
 
 
 if __name__ == "__main__":
