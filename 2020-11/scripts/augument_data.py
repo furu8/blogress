2865c51dfecbba1c28cec399eb32d64298667fa7
furu8 <foooolurk1125@gmail.com>
2021-02-09 03:24:46 +0900

    feat:画像データ拡張スクリプト作成

---
+++ 2020-11/scripts/augument_data.py
@@ -0,0 +1,26 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.datasets import cifar10
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 画像を左右にずらす
+def shift_width():
+    pass
+
+# 画像を上下にずらす
+def shift_width():
+    pass
+
+# 画像を左右反転
+def flip_horizontal():
+    pass
+
+# 画像を回転
+def rotation():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
