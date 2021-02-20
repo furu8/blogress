de4225932b19b6494191e34aa4e6f2df2dcdc1cf
furu8 <foooolurk1125@gmail.com>
2021-02-20 23:54:45 +0900

    ミスった

--- 2020-11/scripts/make_image_data.py
+++ 2020-11/scripts/make_image_data.py
@@ -57,45 +57,11 @@ def make_datagen(rr=30, wsr=0.3, hsr=0.3, hf=True, vf=True):
 
 
 def augument_image(images):
-    image_list = []
-
     datagen = make_datagen()
     for image in images:
         image = image.reshape((1,) + image.shape)
-        for img in datagen.flow(image, batch_size=1):
-           image_list.append(img)
-        break
-
-    return image_list
-
-
-def load_image(path):
-    image_list = []
-    npy_image_list = []
-    image_path_list = gb.glob(path)
-
-    for i, image in enumerate(image_path_list):
-        image_path = image.replace(chr(92), '/') # \を/に置換(windows特有)->macはchr(165)
-        if i % 100 == 0: # 雑に進行状況出力
-            print(i)
-
-        # opencv版（PIL形式が必要なけらばこっちの方が少しだけ早い）
-        # img_npy = cv2.imread(image_path, cv2.IMREAD_COLOR) # デフォルトカラー読み込み
-        # img_npy = cv2.cvtColor(img_npy, cv2.COLOR_BGR2RGB) # RGB変換
-        # img_npy = cv2.resize(img_npy, (64, 64)) # リサイズ64x64
-
-        # keras版
-        img = load_img(image_path, grayscale=False, color_mode='rgb', target_size=(64,64))
-        img_npy = img_to_array(img) / 255
-
-        # plt.imshow(img_npy)
-        # plt.show()
-        # break
-
-        image_list.append(img)
-        npy_image_list.append(img_npy)
-
-    return image_list, npy_image_list
+        for img in datagen.flow(image, batch_size=100):
+            yield img
 
 
 def save_npy_image(save_path, images):
@@ -121,9 +87,9 @@ def main():
 
     # 顔画像
     face_image_list, face_npy_image_list = load_image(FACE_LOAD_PATH)
-    face_aug_image_list = augument_image(face_npy_image_list)
+    # face_aug_image_list = 
     # save_npy_image(FACE_SAVE_PATH, face_npy_image_list)
-    save_image('D:/Illust/Paimon/interim/paimon_face_only_augmentation/', FACE_LOAD_PATH, face_aug_image_list)
+    save_image('D:/Illust/Paimon/interim/paimon_face_only_augmentation/', FACE_LOAD_PATH, augument_image(face_npy_image_list))
 
     # 飯画像
     # food_image_list, food_npy_image_list = load_image(FOOD_LOAD_PATH)
