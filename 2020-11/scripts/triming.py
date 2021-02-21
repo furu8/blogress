7cf16de6cc4c54e1942cc8c425f54335da2bdfb3
furu8 <foooolurk1125@gmail.com>
2021-02-21 13:14:57 +0900

    feat:標準入力で顔検知の精度指定

--- 2020-11/scripts/triming.py
+++ 2020-11/scripts/triming.py
@@ -3,7 +3,7 @@ import cv2
 import glob as gb
 
 # 顔抽出
-def get_face_img(image):
+def get_face_img(image, mode):
     # animeface
     cascade = cv2.CascadeClassifier('../config/lbpcascade_animeface.xml')
     
@@ -11,10 +11,16 @@ def get_face_img(image):
     gray_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY) # グレースケール化
     gray_image = cv2.equalizeHist(gray_image) # ヒストグラム平均化（見やすくなる）
     
-    face_list = cascade.detectMultiScale(gray_image, scaleFactor=1.01, minNeighbors=5, minSize=(24, 24)) # 見逃しを極力少なくパラメータ設定した場合
-    # face_list = cascade.detectMultiScale(gray_image, scaleFactor=1.09, minNeighbors=5, minSize=(24, 24)) # 誤検知を極力少なくパラメータ設定した場合
+    if mode == '0':
+        face_list = cascade.detectMultiScale(gray_image, scaleFactor=1.01, minNeighbors=5, minSize=(24, 24)) # 見逃しを極力少なくパラメータ設定した場合
+    elif mode == '1':
+        face_list = cascade.detectMultiScale(gray_image, scaleFactor=1.09, minNeighbors=5, minSize=(24, 24)) # 誤検知を極力少なくパラメータ設定した場合
+    else:
+        print('入力が不正です（0:見逃し少 1:誤検知少）')
+        face_list = None
     
     return face_image, face_list
+
 
 def save_face_image(save_path, face_img):
     if not os.path.exists(save_path): # DL済みの画像かどうか判定
@@ -24,14 +30,18 @@ def save_face_image(save_path, face_img):
         cv2.imwrite(save_path, face_img) 
         print(save_path + 'をトリミングし保存しました')
 
+
+# リサイズ関数（使っていない）
 # def resize_face_illust(face_img, size):
 #     width, height = size, size
 #     resize_face_img = cv2.resize(face_img, (width, height))
     
 #     return resize_face_img
 
+
 # トリミング実行
 def main():
+    mode = input('mode 0:見逃し少 1:誤検知少 >')
     # トリミング対象の画像リスト
     image_list = gb.glob('D:/Illust/Paimon/raw/*')
     # print(len(image_list))
@@ -42,7 +52,9 @@ def main():
        
         # 顔抽出
         try:
-            face_image, face_list = get_face_img(image)
+            face_image, face_list = get_face_img(image, mode)
+            if face_list is None:
+                break
         except:
             print(save_file_name)
             print(image)
@@ -54,7 +66,7 @@ def main():
             x, y, w, h = face # x始点、y始点、w幅、h高さ
             face_img = face_image[y:y+h, x:x+w] # トリミング
             # pngで保存
-            save_path = 'D:/Illust/Paimon/interim/face/' + save_file_name + '_' +str(i) + '.png'
+            save_path = 'D:/Illust/Paimon/interim/face_org/' + save_file_name + '_' +str(i) + '.png'
             save_face_image(save_path, face_img)
 
 
