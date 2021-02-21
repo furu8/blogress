b6eaafb88b9edaab08c5065eb0f3f55250329989
furu8 <foooolurk1125@gmail.com>
2021-02-21 19:22:10 +0900

    feat:二次元と三次元散布図可視化機能追加

--- 2020-11/scripts/clustering.py
+++ 2020-11/scripts/clustering.py
@@ -5,11 +5,13 @@ import cv2
 import os
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def load_image(path):
@@ -36,30 +38,62 @@ def load_image(path):
     return npy_image_list
 
 
-def build_kmeans(cluster_num, df):
-    model = KMeans(n_clusters=cluster_num)
-    model.fit(df)
+def build_kmeans(df, cluster_num):
+    kmeans = KMeans(n_clusters=cluster_num)
+    kmeans.fit(df)
 
-    return model
+    return kmeans
 
 
-def plot_variance_ratio(pca):
+def build_pca(df):
+    pca = PCA()
+    pca.fit(df)
+
+    return pca
+    
+
+def plot_contribution_rate(pca):
     plt.gca().get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
-    plt.plot([0] + list( np.cumsum(pca.explained_variance_ratio_)), "-o")
+    plt.plot([0] + list(np.cumsum(pca.explained_variance_ratio_)), "-o")
     plt.xlabel("Number of principal components")
     plt.ylabel("Cumulative contribution rate")
     plt.grid()
     plt.show()
 
 
+def plot_scatter2d(df):
+    sns.scatterplot(data=df, x='PC1', y='PC2', hue='label', palette='bright', legend='full')
+    plt.show()
+    # plt.savefig('figure/pca_scatter2d.png')
+
+
+def plot_scatter3d(df):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    #軸にラベルを付けたいときは書く
+    ax.set_xlabel("PC1")
+    ax.set_ylabel("PC2")
+    ax.set_zlabel("PC3")
+
+    #.plotで描画
+    for label in df['label'].values:
+        ax.plot(df.loc[df['label']==label, 'PC1'], 
+                df.loc[df['label']==label, 'PC2'], 
+                df.loc[df['label']==label, 'PC3'], 
+                alpha=0.8, marker=".", linestyle='None')
+    plt.show()
+    # plt.savefig('figure/pca_scatter3d.png')
+
+
 # 結果をクラスタごとにディレクトリに保存
-def make_cluster_dir(load_path, save_path, model):
+def make_cluster_dir(load_path, save_path, kmeans):
     # 保存先のディレクトリを空にして作成
     shutil.rmtree(save_path)
     os.mkdir(save_path)
 
     # クラスタごとのディレクトリ作成
-    for i in range(model.n_clusters):
+    for i in range(kmeans.n_clusters):
         cluster_dir = save_path + "cluster{}".format(i)
         if os.path.exists(cluster_dir):
             shutil.rmtree(cluster_dir)
@@ -67,34 +101,40 @@ def make_cluster_dir(load_path, save_path, model):
 
     # 各ディレクトリにコピー保存
     image_path_list = gb.glob(load_path)
-    for label, path in zip(model.labels_, image_path_list):
+    for label, path in zip(kmeans.labels_, image_path_list):
         shutil.copyfile(path, save_path + 'cluster{}/{}'.format(label, os.path.basename(path)))
+
+    print('クラスタごとにファイル作成完了')
+
 
 def main():
     LOAD_PATH = 'D:/Illust/Paimon/interim/face_only/*'
     SAVE_PATH = 'D:/Illust/Paimon/interim/face_only_clustering/'
-    # npy_image_list = load_image(LOAD_PATH)
-    # df = pd.DataFrame(npy_image_list)
-    # print(df.shape)
+    CSV_PATH = 'D:/Illust/Paimon/interim/face_only_pca.csv'       # 画像データを主成分分析した結果の保存先
     
-    # 主成分分析の実行
-    # pca = PCA()
-    # pca.fit(df)
-    # feature = pca.transform(df)
-    # print(feature.shape)
-    # pca_df = pd.DataFrame(feature, columns=["PC{}".format(x + 1) for x in range(len(df))])
-    # plot_variance_ratio(pca)
-    # pca_df.to_csv('D:/Illust/Paimon/interim/face_only_pca.csv', index=False)
-
-    pca_df = pd.read_csv('D:/Illust/Paimon/interim/face_only_pca.csv')
+    try:
+        pca_df = pd.read_csv(CSV_PATH)
+    except FileNotFoundError:
+        # 画像読み込み
+        npy_image_list = load_image(LOAD_PATH)
+        df = pd.DataFrame(npy_image_list)
+        print(df.shape)
+        
+        # 主成分分析の実行
+        pca = build_pca(df)
+        pca_df = pd.DataFrame(pca.transform(df), columns=["PC{}".format(x + 1) for x in range(len(df))])
+        plot_contribution_rate(pca) # 累積寄与率可視化
+        pca_df.to_csv(CSV_PATH, index=False) # 保存
+    
     train_df = pca_df.iloc[:, :1200]
+    
+    cluster_num = int(input('cluster_num >')) # クラスタ数を入力
+    kmeans = build_kmeans(train_df, cluster_num) # kmeansモデル構築
+    make_cluster_dir(LOAD_PATH, SAVE_PATH, kmeans) # クラスタリング結果からディレクトリ作成
 
-    cluster_num = input('cluster_num >') 
-    model = build_kmeans(cluster_num, train_df)
-    # plt.scatter(pca_df['PC1'], pca_df['PC2'], c=model.labels_)
-    sns.scatterplot(data=pca_df, x='PC1', y='PC2', hue=model.labels_)
-    plt.show()
-    make_cluster_dir(LOAD_PATH, SAVE_PATH, model)
+    pca_df['label'] = kmeans.labels_ 
+    plot_scatter2d(pca_df)              # 二次元散布図
+    plot_scatter3d(pca_df)              # 三次元散布図
 
     
 if __name__ == "__main__":
