6c6d2327ca560072961c681dff9f73c69be20129
furu8 <foooolurk1125@gmail.com>
2021-04-10 18:26:15 +0900

    適当に選んだfood画像wo

--- 2021-02/scripts/resize_image_data.py
+++ 2021-02/scripts/resize_image_data.py
@@ -22,11 +22,22 @@ def save_npy_image(save_path, images):
 
 
 face_images = load_image_npy('D:/Illust/Paimon/interim/npy_face_only/paimon_face_augmentation.npy')
-food_images = load_image_npy('D:/OpenData/food-101/interim/npy_food-101.npy')
+food_images = load_image_npy('D:/OpenData/food-101/interim/npy_food-101_64/food/*', isdir=True)
+food_images = np.array(food_images)
+print(food_images.shape)
 
-resize_num = 128
+resize_num = 64
 face_images = np.array([cv2.resize(face_image, (resize_num,resize_num)) for face_image in face_images])
-food_images = np.array([cv2.resize(food_image, (resize_num,resize_num)) for food_image in food_images])
 
-save_npy_image('D:/Illust/Paimon/interim/npy_face_only/paimon_face_augmentation_128.npy', face_images)
-save_npy_image('D:/OpenData/food-101/interim/npy_food-101_128.npy', food_images)
+
+food_img_list = []
+for food_image in food_images:
+    for img in food_image:
+        food_img_list.append(cv2.resize(img, (resize_num,resize_num)))
+
+food_images = np.array(food_img_list)
+print(food_images.shape)
+# food_images = np.array([cv2.resize(food_image, (resize_num,resize_num)) for food_image in food_images])
+
+# save_npy_image('D:/Illust/Paimon/interim/npy_face_only/paimon_face_augmentation_128.npy', face_images)
+save_npy_image('D:/OpenData/food-101/interim/npy_food-101_64/npy_food-101_64_.npy', food_images)
