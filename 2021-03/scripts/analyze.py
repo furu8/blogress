d3185bf13ee1b51eb00b5c76154b7c3d16f52653
furu8 <foooolurk1125@gmail.com>
2021-05-10 16:36:46 +0900

    feat:学習用データ作成

---
+++ 2021-03/scripts/analyze.py
@@ -0,0 +1,134 @@
+
+# %%
+import os
+import pandas as pd
+import numpy as np
+from IPython.core.display import display
+from sklearn.decomposition import PCA
+import umap as up
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import warnings
+# warnings.simplefilter('ignore', pd.core.common.SettingWithCopyWarning)
+
+# %%
+# 読込
+df = pd.read_csv('../data/interim/all.csv')
+df.head()
+
+# %%[markdown]
+# ## 初期の可視化
+
+# # %%
+# # 関数
+# def plot_bar(df1, df2, col, xlabels):
+#     left = df1[col].value_counts(sort=False) / len(df1)
+#     right = df2[col].value_counts(sort=False) / len(df2)
+
+#     # print(left.index)
+#     # print(right.index)
+#     # print(xlabels)
+#     # labels = np.union1d(left.index.values, right.index.values)
+
+#     fig, ax = plt.subplots(1, 2, figsize=(6, 4))
+#     ax[0].bar(left.index, left, color='red')     # 左
+#     ax[1].bar(right.index, right, color='green') # 右
+
+#     ax[0].set_xticks(left.index)
+#     ax[0].set_xticklabels(xlabels)
+#     ax[1].set_xticks(left.index)
+#     ax[1].set_xticklabels(xlabels)
+#     ax[0].set_ylim(0,1)
+#     ax[1].set_ylim(0,1)
+#     plt.show()
+
+# # %%
+# # 死亡者と生存者の違い
+# df_s0 = df[df['Survived']==0] # 死亡者
+# df_s1 = df[df['Survived']==1] # 生存者
+# plot_bar(df_s0, df_s1, 'Pclass', ['1', '2', '3'])
+# plot_bar(df_s0, df_s1, 'Sex', ['female', 'male'])
+# plot_bar(df_s0, df_s1, 'SibSp', ['0', '1', '2', '3', '4', '5', '8'])
+# plot_bar(df_s0, df_s1, 'Parch', ['0', '1', '2', '3', '4', '5', '6'])
+# plot_bar(df_s0, df_s1, 'Embarked', ['C', 'Q', 'S'])
+
+# # %%
+# # trainとtestの違い
+# df_s0 = df[df['Survived']!=-1] # train
+# df_s1 = df[df['Survived']==-1] # test
+# plot_bar(df_s0, df_s1, 'Pclass', ['1', '2', '3'])
+# plot_bar(df_s0, df_s1, 'Sex', ['female', 'male'])
+# plot_bar(df_s0, df_s1, 'SibSp', ['0', '1', '2', '3', '4', '5', '8'])
+# plot_bar(df_s0, df_s1, 'Parch', ['0', '1', '2', '3', '4', '5', '6'])
+# plot_bar(df_s0, df_s1, 'Embarked', ['C', 'Q', 'S'])
+
+# %%
+# 死亡者と生存者の違い
+df_s = df[df['data']=='train']
+cols = ['Pclass', 'Sex', 'SibSp', 'Parch', 'Embarked']
+for col in cols:
+    print(col)
+    sns.countplot(x=col, data=df_s, hue=df_s['Survived'])
+    plt.legend( loc='upper right')
+    plt.show()
+
+# %%
+# trainとtestの違い
+cols = ['Pclass', 'Sex', 'SibSp', 'Parch', 'Embarked']
+for col in cols:
+    print(col)
+    sns.countplot(x=col, data=df, hue=df['data'])
+    plt.legend( loc='upper right')
+    plt.show()
+
+# %%[markdown]
+# ## 特徴量を追加
+
+# %%
+# 家族人数
+df['Family'] = df['SibSp'] + df['Parch']
+
+# ラベルエンコーディング
+lenc = LabelEncoder()
+
+lenc.fit(df['Sex'])
+df['Sex'] = pd.DataFrame(lenc.transform(df['Sex']))
+
+lenc.fit(df['Embarked'])
+df['Embarked'] = pd.DataFrame(lenc.transform(df['Embarked']))
+
+df['Cabin'] = df['Cabin'].apply(lambda x:str(x)[0])
+lenc.fit(df['Cabin'])
+df['Cabin'] = pd.DataFrame(lenc.transform(df['Cabin']))
+
+display(df)
+display(df.info())
+display(df.describe())
+
+# %%[markdown]
+# ## 特徴量追加後の可視化
+
+# %%
+# 死亡者と生存者の違い
+df_s = df[df['data']=='train']
+cols = ['Family', 'Cabin']
+for col in cols:
+    print(col)
+    sns.countplot(x=col, data=df_s, hue=df_s['Survived'])
+    plt.legend( loc='upper right')
+    plt.show()
+
+# %%
+# trainとtestの違い
+cols = ['Family', 'Cabin']
+for col in cols:
+    print(col)
+    sns.countplot(x=col, data=df, hue=df['data'])
+    plt.legend( loc='upper right')
+    plt.show()
+
+# %%
+# 保存
+df.to_csv('../data/processed/all.csv')
