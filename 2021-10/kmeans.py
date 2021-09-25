0059334fa5dff2cb023441c8a39c9878a95e5209
furu8 <foooolurk1125@gmail.com>
2021-09-25 18:13:19 +0900

    あれいけた？

--- 2021-10/kmeans.py
+++ 2021-10/kmeans.py
@@ -15,67 +15,55 @@ df
 # df
 
 # %%
-plt.figure(figsize=(20,4))
-plt.plot(df['Acc_X'])
-plt.show()
+sensors = [
+    'Acc_X', 'Acc_Y', 'Acc_Z',
+    'Roll', 'Pitch', 'Yaw',
+    'Com_X', 'Com_Y', 'Com_Z'
+]
+# %%
+for sensor in sensors:
+    plt.figure(figsize=(20,4))
+    plt.title(sensor)
+    plt.plot(df[sensor])
+    plt.show()
 
 # %%
-scaler = StandardScaler()
-train = scaler.fit_transform(df[['Acc_X']])
-train = train.flatten()
-train
+def scaler(df, sensors):
+    scaler = StandardScaler()
+    arr = scaler.fit_transform(df[sensors])
+    df_std = pd.DataFrame(arr, columns=sensors)
+    return df_std
 
 # %%
-plt.figure(figsize=(20,4))
-plt.plot(train)
-plt.show()
-
+df_std = scaler(df, sensors)
+df_std
 # %%
 window = 128
-subsequences = np.array([train[i:i+window] for i in range(0,len(train)-window+1,3)])
-subsequences.shape
+def make_subsequences(df, sensor):
+    vec = df[sensor].values
+    return np.array([vec[i:i+window] for i in range(len(vec)-window+1)])
 
 # %%
-plt.plot(subsequences[0])
-plt.show()
+for sensor in sensors:
+    plt.figure(figsize=(20,4))
+    plt.title(sensor)
+    subsequences = make_subsequences(df_std, sensor)
+    plt.plot(subsequences[0])
+    plt.show()
 
 # %%
 kmeans = KMeans(n_clusters=3, random_state=2021)
-centers = []
+scaler = StandardScaler()
 
-for i, subseq in enumerate(subsequences):
-    kmeans.fit_predict(subseq.reshape(-1,1))
-    centers.append(kmeans.cluster_centers_.flatten())
-    print(i)
+for sensor in sensors:
+    subsequences = make_subsequences(df_std, sensor)
+    kmeans.fit_predict(subsequences)
+    centers = scaler.fit_transform(kmeans.cluster_centers_)
 
-centers = np.array(centers)
-centers.shape
-
-# %%
-plt.figure(figsize=(20,4))
-for i, center in enumerate(centers):
-    print(i)
-    plt.scatter([i, i+1, i+2], center, color='tab:blue')
-plt.plot(np.sin(range(len(centers))), color='black')
-plt.show()
-
-# %%
-plt.figure(figsize=(20,4))
-plt.scatter([0,1,2], centers[0])
-plt.scatter([3,4,5], centers[1])
-plt.scatter([6,7,8], centers[2])
-plt.scatter([9,10,11], centers[3])
-plt.plot(np.sin(range(12)))
-plt.show()
-# %%
-pred = kmeans.fit_predict(train)
-pred
-# %%
-kmeans.cluster_centers_ 
-
-# %%
-plt.figure(figsize=(20,4))
-plt.plot(train[:,0], train[:,1], alpha=0.5)
-plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_ [:,1], color='r')
-plt.show()
-# %%
+    plt.figure(figsize=(20,4))
+    plt.title(sensor)
+    plt.plot(centers[0], label='cluster1')
+    plt.plot(centers[1], label='cluster2')
+    plt.plot(centers[2], label='cluster3')
+    plt.legend(loc='upper right')
+    plt.show()
